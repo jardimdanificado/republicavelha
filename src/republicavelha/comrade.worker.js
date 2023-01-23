ed77655f12ed09cb246d3fc24749e8f475bf5dc1
Jardel Torres <74869343+jardimdanificado@users.noreply.github.com>
2023-01-23 07:06:20 +0000

    minor change

--- src/republicavelha/comrade.worker.js
+++ src/republicavelha/comrade.worker.js
@@ -1,16 +1,9 @@
 "use strict";
+
 onmessage = async function(event) 
 {
-	if(typeof event.data.modulePath !== 'undefined')
-	{
-		var modul = await import(event.data.modulePath);
-		var args = event.data.args;
-		var option = event.data.method;
-		postMessage(modul[option].apply(null,args));
-	}
-	else if(typeof event.data.method !== 'undefined')
-	{
-		var args = event.data.args;
-		postMessage(event.data.method.apply(null,args));
-	}
+	var modul = await import(event.data.modulePath);
+	var args = event.data.args;
+	var option = event.data.method;
+	postMessage(modul[option].apply(null,args));
 };
