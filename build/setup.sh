9ca049ffb050e911939657285bbae4f1cdf6323f
jardimdanificado <jardeltorreslima01@gmail.com>
2023-03-22 03:47:26 -0300

    setup script updated

--- build/setup.sh
+++ build/setup.sh
@@ -1,21 +1,48 @@
-#this is from https://github.com/pallene-lang/pallene
 #this installs lua 5.4 + luarocks + pallene
+#this is adapted from default pallene install instructions https://github.com/pallene-lang/pallene
+#pallene docs at https://github.com/pallene-lang/pallene/blob/master/doc/manual.md
+#pallene can use lua functions as well
+
+rm -r lua lua-internals luarocks* pallene bin -f
 
 git clone https://www.github.com/pallene-lang/lua-internals/
 cd lua-internals
 make guess -j4
-sudo make install
+make install
 
+mv src ../lua
+cd ../lua
+mkdir bin include include/lua54 lib
+mv *.h ./include/lua54/
+mv *.c ./include/lua54/
+mv *.hpp ./include/lua54/
+mv *.cpp ./include/lua54/
+mv *.o ./lib/
+mv *.a ./lib/
+mv *.awk ./lib/
+mv lua ./bin/
+mv luac ./bin/
+cd bin
+cd ../..
+
+mkdir luarocks
 wget https://luarocks.org/releases/luarocks-3.9.1.tar.gz
 tar xf luarocks-3.9.1.tar.gz
 cd luarocks-3.9.1
-./configure --with-lua=/usr/local
+./configure --prefix=../luarocks/ --with-lua=../lua
 make
-sudo make install
-
-luarocks config local_by_default true
-
-eval "$(luarocks path)"
+make install
+cd ..
+cd luarocks/bin/
+cd ../..
+./luarocks/bin/luarocks config local_by_default true
 
 git clone https://github.com/pallene-lang/pallene/
-luarocks make pallene-dev-1.rockspec
+cd pallene
+../luarocks/bin/luarocks make pallene-dev-1.rockspec --local
+cd ..
+rm -r luarocks-3.9.1 lua-internals *.tar.gz -f
+
+mkdir bin
+ln -rs luarocks/bin/* ./bin/
+ln -rs lua/bin/* ./bin/
