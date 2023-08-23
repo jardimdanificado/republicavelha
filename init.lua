66df9420250fc2f9f57f1a1c7d89d290a4af2626
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-23 18:34:51 -0300

    util > luatils

--- init.lua
+++ init.lua
@@ -1,6 +1,5 @@
 local modulo = {}
 modulo.util = require(mocegui and 'mocegui.luatils' or 'luatils')
-
 modulo.types = require('republicanova.types')
 modulo.terrain = require('republicanova.terrain')
 modulo.plants = modulo.types.plants
