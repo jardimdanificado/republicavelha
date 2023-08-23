353dec72a1a5ae82bfc4594c577b1570e9aa6f30
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-23 20:43:41 -0300

    added back raylib support & luatils updated

--- init.lua
+++ init.lua
@@ -1,5 +1,10 @@
 local modulo = {}
-modulo.util = require('republicanova.luatils')
+if not rl and not love then
+    gl = '21'
+    rl = require('lib.raylib')
+    gl = nil
+end
+modulo.util = require('republicanova.luatils.init')
 modulo.types = require('republicanova.types')
 modulo.terrain = require('republicanova.terrain')
 modulo.plants = modulo.types.plants
