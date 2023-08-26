09246d39ff5ad0993ccfc9abae74c9f9cec11b16
jardimdanificado <jardeltorreslima01@gmail.com>
2023-08-26 06:25:02 -0300

    fix

--- init.lua
+++ init.lua
@@ -1,4 +1,9 @@
 local modulo = {}
+if not rl and not love then
+    gl = '21'
+    rl = require('lib.raylib')
+    gl = nil
+end
 modulo.util = require('republicanova.luatils')
 modulo.types = require('republicanova.types')
 modulo.terrain = require('republicanova.terrain')
