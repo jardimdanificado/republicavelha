cad3fc626f13eede42fd640046b319b74c92f4fb
jardimdanificado <jardeltorreslima01@gmail.com>
2023-11-01 01:09:16 -0300

    save map as json + cjson lua lib added

--- logic.lua
+++ logic.lua
@@ -1,4 +1,4 @@
-local JSON = require('logic.json')
+local cjson = require('cjson')
 -- Republica's core
 local republica = require("logic.init")
 -- options
@@ -20,12 +20,14 @@ function main()
     -- size up to 6 is safe, above 6 you can get buggy maps, default is 2
     -- layers up to 16 are safe, default is 8
     -- generate the world and map
-    print('debug')
+    print('free the waiter')
     io.flush()
     local world = republica.world(options.mapsize,options.mapquality,options.mappolish)
-    io.read()
     
-    io.write(republica.util.stringify(world.map.block[1][1]))
+    io.read()
+    republica.util.file.save.text('debug.txt',cjson.encode(world.map.block))
+    print('ok')
+    --os.exit()
     -- main loop
     while not exit do
         io.flush()
